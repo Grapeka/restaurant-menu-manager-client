38bc5b0523d41119c939c8e4bafd11a485cbbe5a
Grapeka <chotwilaiwan@gmail.com>
2022-12-23 22:55:34 +0700

    feat signup merchant

--- src/pages/signup/Signup.tsx
+++ src/pages/signup/Signup.tsx
@@ -1,44 +1,52 @@
-import React, { useState } from 'react';
-import AppPage from '../../layouts/AppPage/AppPage';
+import React from 'react';
+import AppPage from '../../layouts/page/Page';
+import { Link } from 'react-router-dom';
 
 export default function Signup(): JSX.Element {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [university, setUniversity] = useState('');
-  const [year, setYear] = useState('');
-  const [linkedin, setLinkedin] = useState('');
-  const [github, setGithub] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
     const form = event.target as HTMLFormElement;
 
-    setFirstName((form[0] as HTMLInputElement).value);
-    setLastName((form[1] as HTMLInputElement).value);
-    setUniversity((form[2] as HTMLInputElement).value);
-    setYear((form[3] as HTMLInputElement).value);
-    setLinkedin((form[4] as HTMLInputElement).value);
-    setGithub((form[5] as HTMLInputElement).value);
-    setEmail((form[6] as HTMLInputElement).value);
-    setPassword((form[7] as HTMLInputElement).value);
-    setConfirmPassword((form[8] as HTMLInputElement).value);
+    const name = (form[0] as HTMLInputElement).value;
+    const location = (form[1] as HTMLInputElement).value;
+    const facebookURL = (form[2] as HTMLInputElement).value;
+    const igURL = (form[3] as HTMLInputElement).value;
+    const email = (form[4] as HTMLInputElement).value;
+    const password = (form[5] as HTMLInputElement).value;
+    const confirmPassword = (form[6] as HTMLInputElement).value;
 
-    const data = {
-      firstName,
-      lastName,
-      university,
-      year,
-      linkedin,
-      github,
-      email,
-      password,
-      confirmPassword,
-    };
+    if (password !== confirmPassword) {
+      alert('Password does not match');
+      return;
+    }
 
-    console.log(data);
+    fetch('http://localhost:8000/auth/signup', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        name,
+        location,
+        facebookURL,
+        igURL,
+        email,
+        password,
+      }),
+    })
+      .then((res) => {
+        if (res.status === 403) {
+          alert('Email already exists');
+        } else if (res.status === 200) {
+          // handle successful login
+          alert('Signup successful');
+          return res.json();
+        } else {
+          // handle other status codes
+        }
+      })
+      .catch((error) => {
+        // handle fetch error
+      });
   };
 
   return (
@@ -53,84 +61,56 @@ export default function Signup(): JSX.Element {
           <div className="mb-6 grid gap-6 md:grid-cols-2">
             <div>
               <label
-                htmlFor="first_name"
+                htmlFor="name"
                 className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
               >
-                First name
+                Merchant Name
               </label>
               <input
                 type="text"
-                id="first_name"
+                id="name"
                 className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-                placeholder="John"
+                placeholder="Merchant Name"
               />
             </div>
             <div>
               <label
-                htmlFor="last_name"
+                htmlFor="location"
                 className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
               >
-                Last name
+                Location
               </label>
               <input
                 type="text"
-                id="last_name"
+                id="Location"
                 className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-                placeholder="Doe"
+                placeholder="Bangkok"
               />
             </div>
             <div>
               <label
-                htmlFor="University"
+                htmlFor="facebook URL"
                 className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
               >
-                University
-              </label>
-              <input
-                type="text"
-                id="University"
-                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-                placeholder="KMUTT"
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="Year"
-                className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Year
-              </label>
-              <input
-                type="text"
-                id="Year"
-                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-                placeholder="1"
-              />
-            </div>
-            <div>
-              <label
-                htmlFor=" Linkedin URL"
-                className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Linkedin URL
+                Facebook URL
               </label>
               <input
                 type="url"
-                id=" Linkedin URL"
+                id=" facebook URL"
                 className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
                 placeholder="linkedin.com/in/"
               />
             </div>
             <div>
               <label
-                htmlFor=" github URL"
+                htmlFor="ig URL"
                 className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
               >
-                Github URL
+                Instagram URL
               </label>
               <input
                 type="url"
-                id=" github URL"
+                id="ig URL"
                 className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
                 placeholder="github.com/username/"
               />
@@ -206,9 +186,14 @@ export default function Signup(): JSX.Element {
             type="submit"
             className="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto"
           >
-            Submit
+            Signup
           </button>
         </form>
+        <div className="mt-3 flex w-full cursor-pointer flex-col items-start">
+          <Link to={'/signin'}>
+            <span className="text-xs hover:text-neutral-500">Signin</span>
+          </Link>
+        </div>
       </div>
     </AppPage>
   );
