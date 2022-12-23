4b2e5a82cc4ac3d6737b803e470cd64509c75796
Grapeka <chotwilaiwan@gmail.com>
2022-12-23 22:55:58 +0700

    feat: create apppage layout

--- src/layouts/AppPage/AppPage.tsx
+++ src/layouts/AppPage/AppPage.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import SideBar from '../../components/sideBar/SideBar';
 
-type AppPageProps = {
+type Props = {
   children: React.ReactNode;
 };
 
-export default function AppPage(props: AppPageProps) {
+export default function AppPage(props: Props) {
   return (
-    <div
-      className={
-        'flex h-screen w-full flex-col items-center justify-center bg-gray-100'
-      }
-    >
-      {props.children}
+    <div className={'flex h-screen w-screen flex-row items-center bg-gray-100'}>
+      <SideBar />
+      <div
+        className={
+          'flex h-full w-4/5 flex-col items-center justify-start bg-gray-500'
+        }
+      >
+        {props.children}
+      </div>
     </div>
   );
 }
