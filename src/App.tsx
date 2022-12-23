3bbce9634deb6267ba6edef0aedd41b94dddaba7
Grapeka <chotwilaiwan@gmail.com>
2022-12-23 22:57:15 +0700

    feat: home page route

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Signup from './pages/signup/Signup';
 import Signin from './pages/signin/Signin';
+import Home from './pages/home/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
       </Routes>
