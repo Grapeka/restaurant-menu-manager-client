1061d75d424486fe35a7d702ca13201514bb99e2
Grapeka <chotwilaiwan@gmail.com>
2022-12-23 22:57:05 +0700

    feat: create home page

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react';
+import AppPage from '../../layouts/appPage/AppPage';
+import { IMenu } from '../../types/menu';
+import { menu } from '../../mocks/menu';
+import store from '../../redux/store';
+
+type Props = {};
+
+const body = (menu: IMenu, index: number) => {
+  return (
+    <tr
+      className="border-b bg-white dark:border-gray-700 dark:bg-gray-800"
+      key={index}
+    >
+      <th
+        scope="row"
+        className="whitespace-nowrap py-4 px-6 font-medium text-gray-900 dark:text-white"
+      >
+        {menu.name}
+      </th>
+      <td className="py-4 px-6">{menu.ownerId}</td>
+      <td className="py-4 px-6">{menu.category}</td>
+      <td className="py-4 px-6">{menu.price}$</td>
+      <td className="cursor-pointer py-4 px-6">
+        <img
+          className="h-10 w-10 rounded"
+          src={
+            menu.image !== ''
+              ? menu.image
+              : 'https://www.eatthis.com/wp-content/uploads/sites/4/2020/12/unhealthiest-foods-planet.jpg?quality=82&strip=1'
+          }
+          alt="Food"
+        />
+      </td>
+    </tr>
+  );
+};
+
+export default function Home(props: Props) {
+  const state = store.getState();
+  const merchant = state.merchant;
+
+  console.log(merchant);
+
+  const [menus, setMenus] = useState<IMenu[]>(menu);
+  return (
+    <AppPage>
+      <div className="relative mt-9 w-5/6 overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400">
+          <thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="py-3 px-6">
+                Menu name
+              </th>
+              <th scope="col" className="py-3 px-6">
+                owner id
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Category
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Price
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Image
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {menus.map((menu, index) => {
+              return body(menu, index);
+            })}
+          </tbody>
+        </table>
+      </div>
+    </AppPage>
+  );
+}
