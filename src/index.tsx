02c3bc1f286cb432e389766ac6dac4611ca6dd20
Grapeka <chotwilaiwan@gmail.com>
2022-12-25 14:13:01 +0700

    feat: setup provider

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+let persistor = persistStore(store);
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
