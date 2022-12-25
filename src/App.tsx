a3dd546ef95d3e37d53ec9ab7d7ae19fdcab529b
Grapeka <chotwilaiwan@gmail.com>
2022-12-25 13:02:08 +0700

    feat: add route my-menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import MyMenus from './pages/myMenus/MyMenus';
 import Signup from './pages/signup/Signup';
 import Signin from './pages/signin/Signin';
 import Home from './pages/home/Home';
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/my-menus" element={<MyMenus />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
       </Routes>
