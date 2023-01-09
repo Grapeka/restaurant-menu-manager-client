ee152616e651637d7ba3d7282a5b613c1d15b70c
Grapeka <chotwilaiwan@gmail.com>
2023-01-10 00:41:04 +0700

    `feat` : create sidebar menu logic

--- src/components/sideBar/SideBar.tsx
+++ src/components/sideBar/SideBar.tsx
@@ -1,4 +1,52 @@
+import { useState, useEffect } from 'react';
+import { WEB_URL } from '../../config';
+import { logout } from '../../utils/logout';
+import { store } from '../../redux/store';
 export default function SideBar() {
+  const state = store.getState();
+  const merchant = state.merchant;
+  const auth = state.auth;
+
+  const [sideBarMenu, setSideBarMenu] = useState({
+    AllMenus: true,
+    MyMenus: true,
+    Merchant: true,
+    Signin: true,
+    Signup: true,
+    Logout: false,
+  });
+
+  useEffect(() => {
+    // check merchant or not
+    if (auth.token === '') {
+      setSideBarMenu({
+        AllMenus: true,
+        MyMenus: false,
+        Merchant: false,
+        Signin: true,
+        Signup: true,
+        Logout: false,
+      });
+    } else {
+      console.log({
+        AllMenus: true,
+        MyMenus: true,
+        Merchant: true,
+        Signin: false,
+        Signup: false,
+        Logout: true,
+      });
+
+      setSideBarMenu({
+        AllMenus: true,
+        MyMenus: true,
+        Merchant: true,
+        Signin: false,
+        Signup: false,
+        Logout: true,
+      });
+    }
+  }, []);
   return (
     <aside className="h-full w-1/5 max-w-xs bg-gray-50" aria-label="Sidebar">
       <div className="mt-7 h-full overflow-y-auto rounded px-3">
@@ -20,7 +68,7 @@ export default function SideBar() {
             <a
               href="/"
               className={`flex items-center rounded-lg p-2 text-base font-normal text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 ${
-                window.location.href === 'http://localhost:3000/'
+                window.location.href === WEB_URL + '/'
                   ? 'bg-gray-100 dark:bg-gray-700'
                   : ''
               }`}
@@ -43,9 +91,9 @@ export default function SideBar() {
           </li>
           <li>
             <a
-              href="/my-menus"
+              href={sideBarMenu.MyMenus === true ? '/my-menus' : '/signin'}
               className={`flex items-center rounded-lg p-2 text-base font-normal text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 ${
-                window.location.href === 'http://localhost:3000/MyMenus'
+                window.location.href === WEB_URL + '/my-menus'
                   ? 'bg-gray-100 dark:bg-gray-700'
                   : ''
               }`}
@@ -68,7 +116,7 @@ export default function SideBar() {
           </li>
           <li>
             <a
-              href="/Merchant"
+              href={sideBarMenu.Merchant ? '/merchant' : '/signin'}
               className={`flex items-center rounded-lg p-2 text-base font-normal text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 ${
                 window.location.href === 'http://localhost:3000/Merchant'
                   ? 'bg-gray-100 dark:bg-gray-700'
@@ -91,48 +139,78 @@ export default function SideBar() {
               <span className="ml-3 flex-1 whitespace-nowrap">Merchant</span>
             </a>
           </li>
-          <li>
-            <a
-              href="/signin"
-              className="flex items-center rounded-lg p-2 text-base font-normal text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+          {sideBarMenu.Signin ? (
+            <>
+              <li>
+                <a
+                  href="/signin"
+                  className="flex items-center rounded-lg p-2 text-base font-normal text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                >
+                  <svg
+                    aria-hidden="true"
+                    className="h-6 w-6 flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <span className="ml-3 flex-1 whitespace-nowrap">Sign In</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/signup"
+                  className="flex items-center rounded-lg p-2 text-base font-normal text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                >
+                  <svg
+                    aria-hidden="true"
+                    className="h-6 w-6 flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <span className="ml-3 flex-1 whitespace-nowrap">Sign Up</span>
+                </a>
+              </li>
+            </>
+          ) : (
+            <li
+              onClick={() => {
+                logout();
+              }}
             >
-              <svg
-                aria-hidden="true"
-                className="h-6 w-6 flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
+              <a
+                href="/signin"
+                className="flex items-center rounded-lg p-2 text-base font-normal text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
               >
-                <path
-                  fillRule="evenodd"
-                  d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              <span className="ml-3 flex-1 whitespace-nowrap">Sign In</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="/signup"
-              className="flex items-center rounded-lg p-2 text-base font-normal text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-            >
-              <svg
-                aria-hidden="true"
-                className="h-6 w-6 flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              <span className="ml-3 flex-1 whitespace-nowrap">Sign Up</span>
-            </a>
-          </li>
+                <svg
+                  aria-hidden="true"
+                  className="h-6 w-6 flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <span className="ml-3 flex-1 whitespace-nowrap">Logout</span>
+              </a>
+            </li>
+          )}
         </ul>
       </div>
     </aside>
