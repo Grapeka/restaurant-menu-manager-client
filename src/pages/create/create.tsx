e86446dd482dc8369fdb8fa078d9b62dd2e1ac96
Grapeka <chotwilaiwan@gmail.com>
2023-01-10 20:43:56 +0700

    `feat` : remove selected

--- src/pages/create/create.tsx
+++ src/pages/create/create.tsx
@@ -174,9 +174,7 @@ export default function Create() {
                 required
                 className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
               >
-                <option selected value="Main dishes">
-                  Main dishes
-                </option>
+                <option value="Main dishes">Main dishes</option>
                 <option value="Appetizers">Appetizers</option>
                 <option value="Desserts">Desserts</option>
                 <option value="Beverages">Beverages</option>
